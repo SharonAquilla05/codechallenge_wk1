a001e722e38c91368ca6d1a67c2e98dabe2d9f0f
Sharon <Sharonaquilla28@gmail.com>
2024-03-23 16:19:13 +0300

    adds grade list

---
+++ script.js
@@ -0,0 +1,25 @@
+const grade =prompt ("Enter Your Grade:");
+UserGrade(grade);
+
+
+
+function UserGrade (userInput){
+    let finalGrade = "E";
+    
+       if(userInput >=0 && userInput <40){
+        finalGrade = "E";
+       }
+       else if(userInput>=40 && userInput< 49){
+        finalGrade = "D";
+       }
+       else if(userInput>=49 && userInput<=59){
+        finalGrade = "C";}
+        else if(userInput>=60 && userInput <=79){
+            finalGrade = "B";
+        }
+        else if(userInput>=79 && userInput <=100){
+            finalGrade ="A";
+        }
+        return finalGrade;
+}
+Document.get.ElementaryById("response").innerText = finalGrade;
